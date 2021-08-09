0182a53f55d0f9e0a0cc4aca061cb9312b5b746f
rizalord <ahmadkhamdani9@gmail.com>
2021-08-09 14:19:59 +0700

    change app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,12 @@ const init = async () => {
 
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost'
+        host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*']
+            }
+        }
     });
 
     server.route(routes);
